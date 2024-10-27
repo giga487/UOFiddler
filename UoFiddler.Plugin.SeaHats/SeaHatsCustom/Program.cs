44a8f2d05bbfe59aaaa9221372b37d642e557010
giga487 <giga487@hotmail.com>
2024-10-27 18:32:52 +0100

    update

---
+++ UoFiddler.Plugin.SeaHats/SeaHatsCustom/Program.cs
@@ -0,0 +1,28 @@
+// /***************************************************************************
+//  *
+//  * $Author: Turley
+//  * 
+//  * "THE BEER-WARE LICENSE"
+//  * As long as you retain this notice you can do whatever you want with 
+//  * this stuff. If we meet some day, and you think this stuff is worth it,
+//  * you can buy me a beer in return.
+//  *
+//  ***************************************************************************/
+
+namespace SeaHatsCustom
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new SeaHatsPluginForm());
+        }
+    }
+}
---
+++ UoFiddler.Plugin.SeaHats/SeaHatsCustom/SeaHatsPluginBase.cs
@@ -0,0 +1,68 @@
+﻿// /***************************************************************************
+//  *
+//  * $Author: Turley
+//  * 
+//  * "THE BEER-WARE LICENSE"
+//  * As long as you retain this notice you can do whatever you want with 
+//  * this stuff. If we meet some day, and you think this stuff is worth it,
+//  * you can buy me a beer in return.
+//  *
+//  ***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using SeaHatsCustom;
+using Ultima;
+using UoFiddler.Controls.Classes;
+using UoFiddler.Controls.Plugin;
+using UoFiddler.Controls.Plugin.Interfaces;
+
+namespace UoFiddler.Plugin.SeaHats
+{
+    public class SeaHatsPluginBase : PluginBase
+    {
+        SeaHatsPluginForm? _form { get; set; } = null;
+        public override IPluginHost Host { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override string Name => "SeaHats Custom Plugin";
+
+        public override string Description => "Plugin for everything we need";
+
+        public override string Author => "Giga487";
+
+        public override string Version => "0.0.1";
+
+        public override void Initialize()
+        {
+            _form = new SeaHatsPluginForm();
+
+        }
+
+        public override void ModifyTabPages(System.Windows.Forms.TabControl tabControl)
+        {
+            TabPage page = new TabPage
+            {
+                Tag = tabControl.TabCount + 1, // at end used for undock/dock feature to define the order
+                Text = "Multi Editor"
+            };
+
+            _form = new SeaHatsPluginForm()
+            {
+                Dock = DockStyle.Fill
+            };
+            page.Controls.Add(_form);
+
+            tabControl.TabPages.Add(page);
+        }
+
+        public override void Unload()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
